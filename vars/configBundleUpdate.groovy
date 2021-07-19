e10863ec0941ce6d3df68bf0e984d39a93fd33cb
Kurt Madel <kmadel@cloudbees.com>
2021-07-19 09:31:43 -0400

    Update configBundleUpdate.groovy

--- vars/configBundleUpdate.groovy
+++ vars/configBundleUpdate.groovy
@@ -1,6 +1,6 @@
 // vars/configBundleUpdate.groovy
 def call(String nameSpace = "sda") {
-  def masterName = System.properties.'MASTER_NAME'
+  def bundleName = env.BUNDLE_ID
   def label = "kubectl"
   def podYaml = libraryResource 'podtemplates/kubectl.yml'
   
@@ -8,16 +8,16 @@ def call(String nameSpace = "sda") {
     node(label) {
       checkout scm
       container("kubectl") {
-        sh "mkdir -p ${masterName}"
-        sh "cp *.yaml ${masterName}"
-        sh "kubectl cp --namespace ${nameSpace} ${masterName} cjoc-0:/var/jenkins_home/jcasc-bundles-store/ -c jenkins"
+        sh "mkdir -p ${bundleName}"
+        sh "cp *.yaml ${bundleName}"
+        sh "kubectl cp --namespace ${nameSpace} ${bundleName} cjoc-0:/var/jenkins_home/jcasc-bundles-store/ -c jenkins"
       }
       echo "begin config bundle reload"
       sh "curl -O https://raw.githubusercontent.com/cloudbees-days/ops-workshop-setup/master/groovy/reload-casc.groovy"
-      sh "curl -O http://teams-${masterName}/teams-${masterName}/jnlpJars/jenkins-cli.jar"
+      sh "curl -O http://${bundleName}/${bundleName}/jnlpJars/jenkins-cli.jar"
       withCredentials([usernamePassword(credentialsId: 'admin-cli-token', usernameVariable: 'JENKINS_CLI_USR', passwordVariable: 'JENKINS_CLI_PSW')]) {
           sh """
-            alias cli='java -jar jenkins-cli.jar -s http://teams-${masterName}/teams-${masterName}/ -auth $JENKINS_CLI_USR:$JENKINS_CLI_PSW'
+            alias cli='java -jar jenkins-cli.jar -s http://${bundleName}/${bundleName}/ -auth $JENKINS_CLI_USR:$JENKINS_CLI_PSW'
             cli groovy =<./reload-casc.groovy
           """
       }
