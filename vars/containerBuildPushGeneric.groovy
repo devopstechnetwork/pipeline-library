ee38d410b48b51612e0c7d75f7a00d7409858381
Kurt Madel <kmadel@mac.com>
2022-02-07 12:37:06 -0500

    Update containerBuildPushGeneric.groovy

--- vars/containerBuildPushGeneric.groovy
+++ vars/containerBuildPushGeneric.groovy
@@ -1,23 +1,30 @@
 // vars/containerBuildPushGeneric.groovy
-def call(String imageName, 
-         String containerRegistry = "us-east1-docker.pkg.dev/core-workshop/workshop-registry", 
-         Closure body) {
+def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject = "core-workshop", Closure body) {
+  def dockerReg = "gcr.io/${gcpProject}"
   def label = "kaniko-${UUID.randomUUID().toString()}"
   def podYaml = libraryResource 'podtemplates/kaniko.yml'
   def customBuildArg = ""
-  podTemplate(name: 'kaniko', inheritFrom: 'default-jnlp', label: label, yaml: podYaml, podRetention: never(), activeDeadlineSeconds:1) {
+  def buildModeArg = ""
+  podTemplate(name: 'kaniko', label: label, yaml: podYaml, podRetention: never(), activeDeadlineSeconds:1) {
     node(label) {
       body()
-      def tag = env.IMAGE_TAG ?: env.SHORT_COMMIT
-      if(env.EVENT_BASE_IMAGE_TAG) {
-        customBuildArg = "--build-arg BASE_IMAGE_TAG=${env.EVENT_BASE_IMAGE_TAG}"
+      try {
+        env.VERSION = readFile 'version.txt'
+        env.VERSION = env.VERSION.trim()
+        env.VERSION = "${env.VERSION}-${BUILD_NUMBER}"
+        imageTag = env.VERSION
+      } catch(e) {}
+      if(env.EVENT_PUSH_IMAGE_TAG) {
+        customBuildArg = "--build-arg BASE_IMAGE=${env.EVENT_PUSH_IMAGE_NAME}:${env.EVENT_PUSH_IMAGE_TAG}"
+      }
+      if(env.BRANCH_NAME != "master") {
+        buildModeArg = "--build-arg BUILD_MODE=build:dev" 
       }
       imageName = imageName.toLowerCase()
       container(name: 'kaniko', shell: '/busybox/sh') {
         withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
-          sh label: "kaniko build and push container image", script: """#!/busybox/sh
-            /kaniko/docker-credential-gcr configure-docker --registries=${containerRegistry}
-            /kaniko/executor -f ${pwd()}/Dockerfile -c ${pwd()} ${customBuildArg} --build-arg COMMIT_AUTHOR='${COMMIT_AUTHOR}' --build-arg GIT_COMMIT=${env.COMMIT_SHA} --cache=true -d ${containerRegistry}/${imageName}:${tag}
+          sh label: "container build and push", script: """#!/busybox/sh
+            /kaniko/executor -f ${pwd()}/Dockerfile -c ${pwd()} ${buildModeArg} ${customBuildArg} --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --cache=true -d ${dockerReg}/${imageName}:${imageTag}
           """
         }
       }
